b0872c30fed9a87692e2b13aa7a9cb4587a31a2f
Rafadcosta <rafaeladuqu@gmail.com>
2020-10-12 16:58:16 -0300

    efeito maquina de escrever

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,22 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-});
+// efeito de máquina de escrever
 
-$('.carousel').carousel({
-    interval: 3000
-});
+function typeWriter(elemento){
+    const textArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textArray.forEach((letra, i) => {
+        setTimeout ( () => elemento.innerHTML += letra, 100 * i)
+    });
+}
+
+const a1 = document.querySelector('.a1');
+const a2 = document.querySelector('.a2');
+const a3 = document.querySelector('.a3');
+const a4 = document.querySelector('.a4');
+const a5 = document.querySelector('.a5');
+typeWriter(a1);
+typeWriter(a2);
+typeWriter(a3);
+typeWriter(a4);
+typeWriter(a5);
+
+// fim efeito de máquina de escrever
