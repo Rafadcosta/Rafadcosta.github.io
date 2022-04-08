5c7ec96e8215fc2808ead24b6fd742193306606d
Rafaela Duque <rafaela@apus.info>
2022-04-07 21:13:45 -0300

    feat: new version website

--- hostelpicus/javascript/scroll.js
+++
@@ -1,8 +0,0 @@
-$('nav a[href^="#"').click(function(e){
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-	$('html, body').animate({
-		scrollTop: targetOffset - 100
-	}, 850);
-});
