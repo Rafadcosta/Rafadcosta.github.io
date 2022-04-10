6208a1735cae8fa4177a2fcdf2b145fda97d58e8
Rafaela Duque <47401318+Rafadcosta@users.noreply.github.com>
2022-04-10 17:11:48 -0300

    feat: caso de estudo

---
+++ hostel-picus/javascript/scroll.js
@@ -0,0 +1,8 @@
+$('nav a[href^="#"').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - 100
+	}, 850);
+});
