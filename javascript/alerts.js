c3c15204d1b53ae6f202513410b5fa95b5e3eeb2
Rafaela Duque <47401318+Rafadcosta@users.noreply.github.com>
2022-04-10 22:12:29 -0300

    feat: alertas portfolio

---
+++ javascript/alerts.js
@@ -0,0 +1,136 @@
+document.querySelector("#jg").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'JG Digital Marketing',    
+    imageUrl: '.././img/portfolio/jg.png',
+    imageAlt: 'JG Digital Marketing',
+    html:
+    '<a target="_blank" href="https://jgdigitalmkt.com/">Acesse o site aqui</a>',
+  })
+});
+
+document.querySelector("#beth").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'Beth Dantas',    
+    imageUrl: '.././img/portfolio/bethdantas.png',
+    imageAlt: 'Beth Dantas',
+    html:
+    '<a target="_blank" href="https://bbethdantas.com/">Acesse o site aqui</a>',
+  })
+});
+
+document.querySelector("#nath").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'Nathalia Batista',    
+    imageUrl: '.././img/portfolio/nathalia.png',
+    imageAlt: 'Nathalia Batista',
+    html:
+    '<a target="_blank" href="https://nathaliabatista.com/">Acesse o site aqui</a>',
+  })
+});
+
+document.querySelector("#shine").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'Shine Fairie Cleaners',    
+    imageUrl: '.././img/portfolio/shinefairies.png',
+    imageAlt: 'Shine Fairie Cleaners',
+    html:
+    '<a target="_blank" href="https://www.shinefairiescleaners.com/">Acesse o site aqui</a>',
+  })
+});
+
+document.querySelector("#anju").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'Anju Mace',    
+    imageUrl: '.././img/portfolio/anju2.png',
+    imageAlt: 'Anju Mace',
+  })
+});
+
+document.querySelector("#newin").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'NewIn',    
+    imageUrl: '.././img/portfolio/newin.png',
+    imageAlt: 'NewIn',
+  })
+});
+
+document.querySelector("#build").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'BuildSmart Construction',    
+    imageUrl: '.././img/portfolio/buildsmart.png',
+    imageAlt: 'BuildSmart Construction',
+    html:
+    '<a target="_blank" href="https://buildsmartconstruction.ca/">Acesse o site aqui</a>',
+  })
+});
+
+document.querySelector("#wiva").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'Wiva Construction',    
+    imageUrl: '.././img/portfolio/wiva.png',
+    imageAlt: 'Wiva Construction',
+    html:
+    '<a target="_blank" href="https://wivaconstruction.com/">Acesse o site aqui</a>',
+  })
+});
+
+document.querySelector("#habitat").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'Habitat Solutions',    
+    imageUrl: '.././img/portfolio/habitat.png',
+    imageAlt: 'Habitat Solutions',
+    html:
+    '<a target="_blank" href="https://www.habitatsolutions.ca/">Acesse o site aqui</a>',
+  })
+});
+
+document.querySelector("#caravan").addEventListener('click', function(){
+    Swal.fire({
+    padding: '1.5em',
+    width: 700,
+    color: '#8C533E',
+    confirmButtonColor: '#8C533E',
+    title: 'Caravan',    
+    imageUrl: '.././img/portfolio/caravan.png',
+    imageAlt: 'Caravan',
+    html:
+    '<a target="_blank" href="https://github.com/Rafadcosta/Caravan/">Acesse o projeto aqui</a>',
+  })
+});
+
