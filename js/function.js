ccb83fd7d5cb1f0cc1ed3673a36ac01a3cb71fc9
Rafaela Duque <rafaeladuqu@gmail.com>
2023-04-04 13:45:44 -0300

    perf: name of function

--- js/function.js
+++ js/function.js
@@ -1,9 +1,9 @@
-function show() {
+function showPhoto() {
     var photo = document.getElementById("about-photo");
     photo.classList.add('show');
 }
 
-function hide(){
+function hidePhoto(){
     var hide = document.getElementById("about-photo");
     hide.classList.remove('show');
 }
