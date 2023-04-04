d21619d60d1466405485ac5eab395db4855e5db4
Rafaela Duque <rafaeladuqu@gmail.com>
2023-04-04 13:37:02 -0300

    feat: show/hide photo

---
+++ js/function.js
@@ -0,0 +1,10 @@
+function show() {
+    var photo = document.getElementById("about-photo");
+    photo.classList.add('show');
+}
+
+function hide(){
+    var hide = document.getElementById("about-photo");
+    hide.classList.remove('show');
+}
+
